a6b2fa9214c37dcfb6288e980b28be338731fbc7
Akarsh E.R <153263317+Akarsher@users.noreply.github.com>
2025-02-20 15:24:28 +0530

    Add files via upload

---
+++ Java/largest.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class largest{
+	
+	static int a=10,b=20,res;
+	
+  public static void main(String args[])
+  {
+    res=(a>b)?a:b;
+    System.out.println("greatest value is"+res);
+  }
+  }
