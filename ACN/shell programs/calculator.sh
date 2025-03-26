9ffbe8e2a742e4a118407b0105b28dcea8feb6f9
Akarsh E.R <153263317+Akarsher@users.noreply.github.com>
2025-03-26 11:36:57 +0530

    Add files via upload

---
+++ ACN/shell programs/calculator.sh
@@ -0,0 +1,30 @@
+echo "simple calculator"
+echo "enter 2 numbers:"
+read a b
+echo "select the operation to perform:"
+s=1
+while [ $s == 1 ]
+do 
+echo "1.addition\n2.subtraction\n3.multiplication\n4.division"
+read n
+
+case $n in
+	1)
+	  echo "sum is " $((a+b))
+	;;
+	2)
+	  echo "difference is " $((a-b))
+	;;
+	3)
+	  echo "product is " $((a*b))
+	;;
+	4)
+	  echo "division is " $((a/b))
+	;;  
+	*)
+	echo -n "Invalid option"
+	;;
+esac	
+echo "do you wish to continue yes(1)/no(0)"
+read s
+done
