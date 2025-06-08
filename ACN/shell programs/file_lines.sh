d263a66e5bf6dc7c968bf4f6d485017e70d883b2
Akarsh E.R <153263317+Akarsher@users.noreply.github.com>
2025-06-08 19:07:02 +0530

    Update file_lines.sh

--- ACN/shell programs/file_lines.sh
+++ ACN/shell programs/file_lines.sh
@@ -1,10 +1,14 @@
-#enter a file name and check if the file exist or not ,if it exist display the lines between 4 & 8 in the file.
+#enter a file name and check if the file exist or not ,if it exist display the lines between 4 & 8 in the file and columns 2 & 3.
 
 read -p "enter a file name: " fname;
 
 if [ -f $fname ] 
 then
+    echo "rows between 4 & 8"
     awk 'NR>4 && NR <8' $fname
+    echo ""
+    echo "columns between 2&3"
+    awk '{print $1,$3}' $fname
 else
     echo "file doesnt exist ."
 fi
